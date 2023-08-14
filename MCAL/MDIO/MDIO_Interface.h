551a33864cd07cbf3116e9f683dda5fec91b2b08
Galal <67324703+galal10@users.noreply.github.com>
2023-08-14 14:40:43 +0300

    Set Port Direction Function

--- MCAL/MDIO/MDIO_Interface.h
+++ MCAL/MDIO/MDIO_Interface.h
@@ -4,5 +4,7 @@
 /* Set Pin Direction Function */
 Error_state MDIO_Error_state_SetPinDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_PinNumber, u8 Copy_u8_PinDirection);
 
+/* Set Port Direction Function */
+Error_state MDIO_Error_state_SetPortDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_PortDirection);
 
 #endif /* MDIO_INTERFACE_H_ */
