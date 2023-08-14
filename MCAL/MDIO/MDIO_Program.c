c1271a9079b3481a9c9db4420dfdda8bf08707df
Galal <67324703+galal10@users.noreply.github.com>
2023-08-14 16:02:18 +0300

    case portA

--- MCAL/MDIO/MDIO_Program.c
+++ MCAL/MDIO/MDIO_Program.c
@@ -301,6 +301,10 @@ Error_state MDIO_Error_state_TogglePinValue(u8 Copy_u8_PortNumber, u8 Copy_u8_Pi
 	{
 		switch( Copy_u8_PortNumber )
 		{
+			case MDIO_PORTA:
+				TOGGLE_BIT(PORTA, Copy_u8_PinNumber);
+				break;
+
 			default:
 				LOC_Error_state_ReturnState = NOK;
 				break;
