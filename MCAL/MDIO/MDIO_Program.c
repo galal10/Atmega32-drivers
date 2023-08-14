417e88b23f9fe30f6ab63fd6eb992b284c375942
Galal <67324703+galal10@users.noreply.github.com>
2023-08-14 15:09:45 +0300

    Update MDIO_Program.c

--- MCAL/MDIO/MDIO_Program.c
+++ MCAL/MDIO/MDIO_Program.c
@@ -95,7 +95,7 @@ Error_state MDIO_Error_state_SetPinDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_P
 Error_state MDIO_Error_state_SetPortDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_PortDirection)
 {
 	Error_state LOC_Error_state_ReturnState = OK;
-
+	
 	if( (Copy_u8_PortDirection == PORT_INPUT) || (Copy_u8_PortDirection == PORT_OUTPUT) )
 	{
 		switch( Copy_u8_PortNumber )
@@ -121,11 +121,11 @@ Error_state MDIO_Error_state_SetPortDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_
 				break;
 		}
 	}
-
+	
 	else
 	{
 		LOC_Error_state_ReturnState = NOK;
 	}
-
+	
 	return LOC_Error_state_ReturnState;
 }
