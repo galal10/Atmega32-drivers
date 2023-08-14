8ea7446e1a9fd049e78ff87ecc160b8f9c1d4a30
Galal <67324703+galal10@users.noreply.github.com>
2023-08-14 14:44:04 +0300

    include LIBS files

--- MCAL/MDIO/MDIO_Interface.h
+++ MCAL/MDIO/MDIO_Interface.h
@@ -1,6 +1,9 @@
 #ifndef MDIO_INTERFACE_H_
 #define MDIO_INTERFACE_H_
 
+#include "../../LIB/LSTD_TYPES.h"
+#include "../../LIB/LBITS.h"
+
 /* Set Pin Direction Function */
 Error_state MDIO_Error_state_SetPinDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_PinNumber, u8 Copy_u8_PinDirection);
 
