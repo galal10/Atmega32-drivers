6ecf04e48062d26578f6c68e944d3a463e4db7c7
Galal <67324703+galal10@users.noreply.github.com>
2023-08-14 14:41:50 +0300

    Set Port Value Function

--- MCAL/MDIO/MDIO_Interface.h
+++ MCAL/MDIO/MDIO_Interface.h
@@ -10,5 +10,7 @@ Error_state MDIO_Error_state_SetPortDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_
 /* Set Pin Value Function */
 Error_state MDIO_Error_state_SetPinValue(u8 Copy_u8_PortNumber, u8 Copy_u8_PinNumber, u8 Copy_u8_PinValue);
 
+/* Set Port Value Function */
+Error_state MDIO_Error_state_SetPortValue(u8 Copy_u8_PortNumber, u8 Copy_u8_PortValue);
 
 #endif /* MDIO_INTERFACE_H_ */
