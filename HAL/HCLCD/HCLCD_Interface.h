3594b1c48956ffaa5aab2d15676dee9cecda956c
Galal <67324703+galal10@users.noreply.github.com>
2023-08-15 20:35:51 +0300

    prototype of Write_Command_4Bits function

--- HAL/HCLCD/HCLCD_Interface.h
+++ HAL/HCLCD/HCLCD_Interface.h
@@ -11,4 +11,6 @@ void HCLCD_Vid_Write_Char_4Bits(u8 Copy_u8_Data);
 
 void HCLCD_Vid_Write_Command_8Bits(u8 Copy_u8_Command);
 
+void HCLCD_Vid_Write_Command_4Bits(u8 Copy_u8_Command);
+
 #endif /* HCLCD_INTERFACE_H_ */
