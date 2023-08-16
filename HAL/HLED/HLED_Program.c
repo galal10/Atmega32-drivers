e87c66fa1a8fcbdf2cd80d4682999ec99ea278fe
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 17:14:28 +0300

    led off function

--- HAL/HLED/HLED_Program.c
+++ HAL/HLED/HLED_Program.c
@@ -8,3 +8,8 @@ void HLED_Vid_Led_on(LED_t *Copy_Led)
 	MDIO_Error_state_SetPinValue(Copy_Led->Copy_u8_Port, Copy_Led->Copy_u8_Pin, PIN_HIGH);
 }
 
+void HLED_Vid_Led_off(LED_t *Copy_Led)
+{
+	MDIO_Error_state_SetPinValue(Copy_Led->Copy_u8_Port, Copy_Led->Copy_u8_Pin, PIN_LOW);
+}
+
