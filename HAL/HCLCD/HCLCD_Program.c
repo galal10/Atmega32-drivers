66625fd8b060fe7d48d0bf48238a963271970b6a
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 16:05:46 +0300

    LCD Write Command 8Bits function

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -1,6 +1,10 @@
 #include "HCLCD_Interface.h"
 #include <util/delay.h>
 
+void HCLCD_Vid_Write_Command_8Bits(u8 Copy_u8_Command)
+{
+}
+
 void HCLCD_Vid_8Bits_Init(void)
 {
 	/* Wait for more than 30ms */
