04e333ba957e291426db594ce994f71c9c19d9c7
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 17:11:46 +0300

    enum for led numbers

--- HAL/HLED/HLED_Interface.h
+++ HAL/HLED/HLED_Interface.h
@@ -1,5 +1,18 @@
 #ifndef HLED_INTERFACE_H_
 #define HLED_INTERFACE_H_
+typedef enum
+{
+	LED1 = PIN0,
+	LED2,
+	LED3,
+	LED4,
+	LED5,
+	LED6,
+	LED7,
+	LED8,
+}E_Led_t;
+
+/* erroes are handled on MCAL so we can set return it void */
 void HLED_Vid_Init(LED_t *Copy_Led);
 
 void HLED_Vid_Led_on(LED_t *Copy_Led);
