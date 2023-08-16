4bdf7e1d30b44ab3c236bf36e9ea023e766c095b
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 16:06:36 +0300

    Send Enable

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -9,6 +9,13 @@ void HCLCD_Vid_Write_Command_8Bits(u8 Copy_u8_Command)
 	MDIO_Error_state_SetPinValue(CONTROL_PORT, RW, PIN_LOW);
 	/* send command on port data */
 	MDIO_Error_state_SetPortValue(DATA_PORT, Copy_u8_Command);
+	/* Send Enable */
+	MDIO_Error_state_SetPinValue(CONTROL_PORT, E, PIN_HIGH);
+	_delay_ms(2);
+	MDIO_Error_state_SetPinValue(CONTROL_PORT, E, PIN_LOW);
+	_delay_ms(2);
+	MDIO_Error_state_SetPinValue(CONTROL_PORT, E, PIN_HIGH);
+
 }
 
 void HCLCD_Vid_8Bits_Init(void)
