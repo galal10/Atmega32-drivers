f5ab61407e81494ec21c341d2911ec15a5e4113b
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 16:06:24 +0300

    send command on port data

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -7,6 +7,8 @@ void HCLCD_Vid_Write_Command_8Bits(u8 Copy_u8_Command)
 	MDIO_Error_state_SetPinValue(CONTROL_PORT, RS, PIN_LOW);
 	/* select Write mode ==> Rw = 0 */
 	MDIO_Error_state_SetPinValue(CONTROL_PORT, RW, PIN_LOW);
+	/* send command on port data */
+	MDIO_Error_state_SetPortValue(DATA_PORT, Copy_u8_Command);
 }
 
 void HCLCD_Vid_8Bits_Init(void)
