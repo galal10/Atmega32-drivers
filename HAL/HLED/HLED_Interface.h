6a85b60f80e5cf02da2545accfec70d9c7fe7f10
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 17:11:23 +0300

    prototype of LED functions

--- HAL/HLED/HLED_Interface.h
+++ HAL/HLED/HLED_Interface.h
@@ -1,3 +1,11 @@
 #ifndef HLED_INTERFACE_H_
 #define HLED_INTERFACE_H_
+void HLED_Vid_Init(LED_t *Copy_Led);
+
+void HLED_Vid_Led_on(LED_t *Copy_Led);
+
+void HLED_Vid_Led_off(LED_t *Copy_Led);
+
+void HLED_Vid_Led_Toggle(LED_t *Copy_Led);
+
 #endif /* HLED_INTERFACE_H_ */
