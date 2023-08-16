2d6e2970637c3d62e421aecdd5b26fd5b54a1f04
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 16:02:04 +0300

    configure Control pins and port

--- HAL/HCLCD/HCLCD_Config.h
+++ HAL/HCLCD/HCLCD_Config.h
@@ -1,4 +1,8 @@
 #ifndef HCLCD_CONFIG_H_
 #define HCLCD_CONFIG_H_
 
+/* Configure RS,RW,E pins ==> (PIN0 to PIN7) */
+#define RS				PIN0
+#define RW				PIN1
+#define E				PIN2
 #endif /* HCLCD_CONFIG_H_ */
