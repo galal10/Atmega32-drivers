dcbb5d596ab7e70e1642a6df3d18ba6aba2da0eb
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 17:14:14 +0300

    led on function

--- HAL/HLED/HLED_Program.c
+++ HAL/HLED/HLED_Program.c
@@ -3,3 +3,8 @@ void HLED_Vid_Init(LED_t *Copy_Led)
 	MDIO_Error_state_SetPinDirection(Copy_Led->Copy_u8_Port, Copy_Led->Copy_u8_Pin, PIN_OUTPUT);
 }
 
+void HLED_Vid_Led_on(LED_t *Copy_Led)
+{
+	MDIO_Error_state_SetPinValue(Copy_Led->Copy_u8_Port, Copy_Led->Copy_u8_Pin, PIN_HIGH);
+}
+
