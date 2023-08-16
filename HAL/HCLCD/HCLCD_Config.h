f1509685aa08b0bcbc7198e78916c43f767da5ae
Galal <67324703+galal10@users.noreply.github.com>
2023-08-16 16:02:31 +0300

    configure control and data port

--- HAL/HCLCD/HCLCD_Config.h
+++ HAL/HCLCD/HCLCD_Config.h
@@ -5,4 +5,10 @@
 #define RS				PIN0
 #define RW				PIN1
 #define E				PIN2
+
+/* Configure Control port ==> (MDIO_PORTA, MDIO_PORTB, MDIO_PORTC, MDIO_PORTD) */
+#define CONTROL_PORT	MDIO_PORTC
+
+/* Configure Data port ==> (MDIO_PORTA, MDIO_PORTB, MDIO_PORTC, MDIO_PORTD) */
+#define DATA_PORT		MDIO_PORTD
 #endif /* HCLCD_CONFIG_H_ */
