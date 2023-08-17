9953907b9d966e054b843352ef1da5c19a0658c1
Galal <67324703+galal10@users.noreply.github.com>
2023-08-17 22:20:25 +0300

    wait 39 µs

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -64,6 +64,8 @@ void HCLCD_Vid_4Bits_Init(void)
 	_delay_ms(1);
 	/* Display ON/OFF command */
 	HCLCD_Vid_Write_Command_4Bits(HCLCD_DISPLAY_ON_OFF);
+	/* wait 39 µs */
+	_delay_ms(1);
 }
 
 void HCLCD_Vid_Write_Char_8Bits(u8 Copy_u8_Data)
