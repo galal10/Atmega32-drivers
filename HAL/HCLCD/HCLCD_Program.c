aaff322f9ebea482ce190c1a0e4c34eddacf4fe5
Galal <67324703+galal10@users.noreply.github.com>
2023-08-17 22:17:19 +0300

    send function set command

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -58,6 +58,8 @@ void HCLCD_Vid_4Bits_Init(void)
 	MDIO_Error_state_SetPinDirection(CONTROL_PORT, E, PIN_OUTPUT);
 	/* Wait for more than 30ms */
 	_delay_ms(30);
+	/* send function set command */
+	HCLCD_Vid_Write_Command_4Bits(HCLCD_FUNCTION_SET);
 }
 
 void HCLCD_Vid_Write_Char_8Bits(u8 Copy_u8_Data)
