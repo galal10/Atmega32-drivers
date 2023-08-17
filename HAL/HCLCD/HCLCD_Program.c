e89f6503834750838bbaa23bda22edaec76baba3
Galal <67324703+galal10@users.noreply.github.com>
2023-08-17 22:22:31 +0300

    select Write mode

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -29,6 +29,9 @@ void HCLCD_Vid_Write_Command_4Bits(u8 Copy_u8_Command)
 {
 	/* select Command register ==> RS = 0 */
 	MDIO_Error_state_SetPinValue(CONTROL_PORT, RS, PIN_LOW);
+	/* select Write mode ==> RW = 0 */
+	MDIO_Error_state_SetPinValue(CONTROL_PORT, RW, PIN_LOW);
+
 }
 
 void HCLCD_Vid_8Bits_Init(void)
