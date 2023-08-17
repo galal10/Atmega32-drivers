45fdb3b5ba898bf992d4d263b720e95d10e53e37
Galal <67324703+galal10@users.noreply.github.com>
2023-08-17 22:24:59 +0300

    Enable function

--- HAL/HCLCD/HCLCD_Program.c
+++ HAL/HCLCD/HCLCD_Program.c
@@ -123,6 +123,8 @@ void HCLCD_Vid_Write_Char_4Bits(u8 Copy_u8_Data)
 	HCLCD_Vid_Kick();
 	/* send 4LSB Data on port data */
 	MDIO_Error_state_SetNibbleValues(DATA_PORT, MODE_4BIT_PINS, (Copy_u8_Data & 0x0F) );
+	/* Enable method */
+	HCLCD_Vid_Kick();
 }
 
 void HCLCD_Vid_Write_String_8Bits(u8 *PCopy_u8_String)
