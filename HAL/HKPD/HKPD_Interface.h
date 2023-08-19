4db5fc51b87bf85fccd7a2061778faa0669bb2db
Galal <67324703+galal10@users.noreply.github.com>
2023-08-19 16:14:36 +0300

    prototype of functions

--- HAL/HKPD/HKPD_Interface.h
+++ HAL/HKPD/HKPD_Interface.h
@@ -1,3 +1,9 @@
 #ifndef HKPD_INTERFACE_H_
 #define HKPD_INTERFACE_H_
+/* KPD init */
+void HKPD_Vid_init(void);
+
+/* KPD Get pressed */
+u8 HKPD_u8_GetKeyPressed(void);
+
 #endif /* HKPD_INTERFACE_H_ */
