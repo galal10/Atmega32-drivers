935118b6e250dcf3109445197a50c0902d61fde2
Galal <67324703+galal10@users.noreply.github.com>
2023-08-19 16:15:41 +0300

    set Row pins as an input

--- HAL/HKPD/HKPD_Program.c
+++ HAL/HKPD/HKPD_Program.c
@@ -1,5 +1,11 @@
 void HKPD_Vid_init(void)
 {
+	/* set Row pins as an input */
+	MDIO_Error_state_SetPinDirection(ROW_PORT, ROW_PIN0, PIN_INPUT);
+	MDIO_Error_state_SetPinDirection(ROW_PORT, ROW_PIN1, PIN_INPUT);
+	MDIO_Error_state_SetPinDirection(ROW_PORT, ROW_PIN2, PIN_INPUT);
+	MDIO_Error_state_SetPinDirection(ROW_PORT, ROW_PIN3, PIN_INPUT);
+
 }
 
 u8 HKPD_u8_GetKeyPressed(void)
