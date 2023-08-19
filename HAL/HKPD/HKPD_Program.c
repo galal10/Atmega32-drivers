c9164fec6f34af7dd7b73f4da4230d7d535566ce
Galal <67324703+galal10@users.noreply.github.com>
2023-08-19 16:15:51 +0300

    set column pins as an output

--- HAL/HKPD/HKPD_Program.c
+++ HAL/HKPD/HKPD_Program.c
@@ -6,6 +6,12 @@ void HKPD_Vid_init(void)
 	MDIO_Error_state_SetPinDirection(ROW_PORT, ROW_PIN2, PIN_INPUT);
 	MDIO_Error_state_SetPinDirection(ROW_PORT, ROW_PIN3, PIN_INPUT);
 
+	/* set column pins as an output */
+	MDIO_Error_state_SetPinDirection(COL_PORT, COL_PIN0, PIN_OUTPUT);
+	MDIO_Error_state_SetPinDirection(COL_PORT, COL_PIN1, PIN_OUTPUT);
+	MDIO_Error_state_SetPinDirection(COL_PORT, COL_PIN2, PIN_OUTPUT);
+	MDIO_Error_state_SetPinDirection(COL_PORT, COL_PIN3, PIN_OUTPUT);
+
 }
 
 u8 HKPD_u8_GetKeyPressed(void)
