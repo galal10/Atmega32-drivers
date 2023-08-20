e7c4d025946d64129613ddde409fe42808d7c295
Galal <67324703+galal10@users.noreply.github.com>
2023-08-20 20:26:37 +0300

    MCUCR bits

--- MCAL/MEXTI/MEXTI_Private.h
+++ MCAL/MEXTI/MEXTI_Private.h
@@ -2,4 +2,12 @@
 #define MEXTI_PRIVATE_H_
 
 /* Control Register : MCUCR */
+#define MCUCR		*((volatile u8*)(0x55))
+/* MCUCR bits */
+#define ISC00							0
+#define ISC01							1
+#define ISC10							2
+#define ISC11							3
+
+
 #endif /* MEXTI_PRIVATE_H_ */
