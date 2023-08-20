87d37b36a36a271245c2e6cce5a142df88ee626e
Galal <67324703+galal10@users.noreply.github.com>
2023-08-20 20:42:48 +0300

    handling error

--- MCAL/MEXTI/MEXTI_Program.c
+++ MCAL/MEXTI/MEXTI_Program.c
@@ -100,5 +100,11 @@ Error_state MEXTI_Vid_SenseControl(u8 Copy_u8_EXTI_ID, MEXTI_SENSE_CONTROL_t Cop
 		if( (Copy_SenseControl >= LOW_LEVEL) && (Copy_SenseControl <= RISING_EDGE) )
 		{
 		}
+		else
+		{
+			LOC_Error_state_ReturnState = NOK;
+		}
+		break;
+
 	case EXTI2:
 }
