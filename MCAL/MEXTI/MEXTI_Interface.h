60e1b0342f78916f40af5f825557448097dd6642
Galal <67324703+galal10@users.noreply.github.com>
2023-08-20 20:30:40 +0300

    Select Sense control Register

--- MCAL/MEXTI/MEXTI_Interface.h
+++ MCAL/MEXTI/MEXTI_Interface.h
@@ -10,5 +10,7 @@ void MEXTI1_Vid_Init(void);
 /* EXTI2 Initialization */
 void MEXTI2_Vid_Init(void);
 
+/* Select Sense control Register */
+Error_state MEXTI_Vid_SenseControl(u8 Copy_u8_EXTI_ID, MEXTI_SENSE_CONTROL_t Copy_SenseControl);
 
 #endif /* MEXTI_INTERFACE_H_ */
