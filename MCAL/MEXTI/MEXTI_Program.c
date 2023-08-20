032345dceeaa25b112f9a25c3f9b54d37900b49e
Galal <67324703+galal10@users.noreply.github.com>
2023-08-20 20:39:45 +0300

    var to return it and end of function

--- MCAL/MEXTI/MEXTI_Program.c
+++ MCAL/MEXTI/MEXTI_Program.c
@@ -80,4 +80,6 @@ void MEXTI2_Vid_Init(void)
 
 /* Select Sense control Register */
 Error_state MEXTI_Vid_SenseControl(u8 Copy_u8_EXTI_ID, MEXTI_SENSE_CONTROL_t Copy_SenseControl)
+{
+	Error_state LOC_Error_state_ReturnState = OK;
 }
