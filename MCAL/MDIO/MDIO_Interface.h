b48e0885c2dca80f5c1fcd5db450f10d71ebc52a
Galal <67324703+galal10@users.noreply.github.com>
2023-08-21 20:35:28 +0300

    Update MDIO_Interface.h

--- MCAL/MDIO/MDIO_Interface.h
+++ MCAL/MDIO/MDIO_Interface.h
@@ -42,6 +42,7 @@ typedef enum
 	PIN7
 }Pin_t;
 
+
 /* Set Pin Direction Function */
 Error_state MDIO_Error_state_SetPinDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_PinNumber, u8 Copy_u8_PinDirection);
 
