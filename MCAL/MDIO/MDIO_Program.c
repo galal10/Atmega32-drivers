57f20249b32f7c0adaa325ef7babcbccef02a4d3
Galal <67324703+galal10@users.noreply.github.com>
2023-08-21 20:36:09 +0300

    modify and reduce lines of code

--- MCAL/MDIO/MDIO_Program.c
+++ MCAL/MDIO/MDIO_Program.c
@@ -4,82 +4,22 @@
 Error_state MDIO_Error_state_SetPinDirection(u8 Copy_u8_PortNumber, u8 Copy_u8_PinNumber, u8 Copy_u8_PinDirection)
 {
 	Error_state LOC_Error_state_ReturnState = OK;
-	
-	if( (Copy_u8_PinNumber >= PIN0) && (Copy_u8_PinNumber <= PIN7) )
-	{	
-		switch( Copy_u8_PortNumber )
+	/* error handling */
+	if( ((Copy_u8_PinNumber >= PIN0) && (Copy_u8_PinNumber <= PIN7)) && ((Copy_u8_PortNumber >= MDIO_PORTA) && (Copy_u8_PortNumber <= MDIO_PORTD)) )
+	{
+		if( Copy_u8_PinDirection == PIN_OUTPUT )
 		{
-			case MDIO_PORTA:
-				if( Copy_u8_PinDirection == PIN_OUTPUT )
-				{
-					SET_BIT(DDRA, Copy_u8_PinNumber);
-				}
-			
-				else if( Copy_u8_PinDirection == PIN_INPUT )
-				{
-					CLR_BIT(DDRA, Copy_u8_PinNumber);
-				}
-			
-				else
-				{
-					LOC_Error_state_ReturnState = NOK;
-				}
-				break;
-				
-			case MDIO_PORTB:
-				if( Copy_u8_PinDirection == PIN_OUTPUT )
-				{
-					SET_BIT(DDRB, Copy_u8_PinNumber);
-				}
-			
-				else if( Copy_u8_PinDirection == PIN_INPUT )
-				{
-					CLR_BIT(DDRB, Copy_u8_PinNumber);
-				}
-			
-				else
-				{
-					LOC_Error_state_ReturnState = NOK;
-				}
-				break;
-				
-			case MDIO_PORTC:
-				if( Copy_u8_PinDirection == PIN_OUTPUT )
-				{
-					SET_BIT(DDRC, Copy_u8_PinNumber);
-				}
-			
-				else if( Copy_u8_PinDirection == PIN_INPUT )
-				{
-					CLR_BIT(DDRC, Copy_u8_PinNumber);
-				}
-			
-				else
-				{
-					LOC_Error_state_ReturnState = NOK;
-				}
-				break;
+			SET_BIT(*DDR[Copy_u8_PortNumber], Copy_u8_PinNumber);
+		}
 
-			case MDIO_PORTD:
-				if( Copy_u8_PinDirection == PIN_OUTPUT )
-				{
-					SET_BIT(DDRD, Copy_u8_PinNumber);
-				}
-			
-				else if( Copy_u8_PinDirection == PIN_INPUT )
-				{
-					CLR_BIT(DDRD, Copy_u8_PinNumber);
-				}
-			
-				else
-				{
-					LOC_Error_state_ReturnState = NOK;
-				}
-				break;
-				
-			default :
-				LOC_Error_state_ReturnState = NOK;
-				break;
+		else if( Copy_u8_PinDirection == PIN_INPUT )
+		{
+			CLR_BIT(*DDR[Copy_u8_PortNumber], Copy_u8_PinNumber);
+		}
+
+		else
+		{
+			LOC_Error_state_ReturnState = NOK;
 		}
 	}
 	
