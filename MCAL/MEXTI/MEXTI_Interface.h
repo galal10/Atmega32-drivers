9bf8ba7df7428a30f8f73f32bb74e452ac743ae5
Galal <67324703+galal10@users.noreply.github.com>
2023-08-21 03:18:03 +0300

    modify parameter type

--- MCAL/MEXTI/MEXTI_Interface.h
+++ MCAL/MEXTI/MEXTI_Interface.h
@@ -26,6 +26,6 @@ void MEXTI1_Vid_Init(void);
 void MEXTI2_Vid_Init(void);
 
 /* Select Sense control Register */
-Error_state MEXTI_Vid_SenseControl(u8 Copy_u8_EXTI_ID, MEXTI_SENSE_CONTROL_t Copy_SenseControl);
+Error_state MEXTI_Vid_SenseControl(MEXTI_EXTI_ID_t Copy_EXTI_ID, MEXTI_SENSE_CONTROL_t Copy_SenseControl);
 
 #endif /* MEXTI_INTERFACE_H_ */
