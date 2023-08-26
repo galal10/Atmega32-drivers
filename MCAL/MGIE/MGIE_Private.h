308f1fb59e58e0d80cbe50f1f3dd31247f0348ca
Galal <67324703+galal10@users.noreply.github.com>
2023-08-26 20:17:12 +0300

    remove files

--- MCAL/MGIE/MGIE_Private.h
+++
@@ -1,9 +0,0 @@
-#ifndef MGIE_PRIVATE_H_
-#define MGIE_PRIVATE_H_
-
-/* AVR status Register */
-#define SREG		*((volatile u8*)(0x5F))
-/* Global Interrupt bit */
-#define INT			7
-
-#endif /* MGIE_PRIVATE_H_ */
