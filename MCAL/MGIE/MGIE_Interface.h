ec27fbe13cfcbe1aea0127e691a320c2f55bdfc6
Galal <67324703+galal10@users.noreply.github.com>
2023-08-26 22:19:43 +0300

    remove unimportant macros

--- MCAL/MGIE/MGIE_Interface.h
+++ MCAL/MGIE/MGIE_Interface.h
@@ -1,14 +1,10 @@
 #ifndef MGIE_INTERFACE_H_
 #define MGIE_INTERFACE_H_
 
-# define sei()  __asm__ __volatile__ ("sei" ::)
-
-# define cli()  __asm__ __volatile__ ("cli" ::)
-
 /*GIE Enable Function*/
-#define MGIE_Vid_Enable() 		sei()
+#define MGIE_Vid_Enable() 		__asm__ __volatile__ ("sei" ::)
 
 /*GIE Disable Function*/
-#define MGIE_Vid_Disable()		cli()
+#define MGIE_Vid_Disable()		__asm__ __volatile__ ("cli" ::)
 
 #endif /* MGIE_INTERFACE_H_ */
