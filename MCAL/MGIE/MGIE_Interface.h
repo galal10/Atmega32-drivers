b57021001959ece559d227dceae03bc2c0ff9f50
Galal <67324703+galal10@users.noreply.github.com>
2023-08-26 20:17:44 +0300

    replace functions by assembly instructions

--- MCAL/MGIE/MGIE_Interface.h
+++ MCAL/MGIE/MGIE_Interface.h
@@ -1,10 +1,14 @@
 #ifndef MGIE_INTERFACE_H_
 #define MGIE_INTERFACE_H_
 
+# define sei()  __asm__ __volatile__ ("sei" ::)
+
+# define cli()  __asm__ __volatile__ ("cli" ::)
+
 /*GIE Enable Function*/
-void MGIE_Vid_Enable(void);
+#define MGIE_Vid_Enable() 		sei()
 
 /*GIE Disable Function*/
-void MGIE_Vid_Disable(void);
+#define MGIE_Vid_Disable()		cli()
 
 #endif /* MGIE_INTERFACE_H_ */
