87e483d6587ed90f3a23b958191e665f4cfa9b76
Galal <67324703+galal10@users.noreply.github.com>
2023-09-07 17:13:31 +0300

    add include files for source file of driver

---
+++ MCAL/MTIMERS/MTIMERS_Program.c
@@ -0,0 +1,14 @@
+/*
+ * MTIMER_Program.c
+ *
+ *  Created on: Aug 25, 2023
+ *      Author: galal
+ */
+
+#include "../../LIB/LBITS.h"
+#include "../../LIB/LSTD_TYPES.h"
+#include "MTIMER0_Config.h"
+#include "MTIMER1_Config.h"
+#include "MTIMER2_Config.h"
+#include "MTIMERS_Interface.h"
+#include "MTIMERS_Private.h"
